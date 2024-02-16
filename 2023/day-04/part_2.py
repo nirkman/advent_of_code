1486b5158ef5564cacb00a817b9d2b96af6c6de0
nirkman <nicholas.kyle@gmail.com>
2024-02-16 00:08:06 -0800

    Day 4, part2

--- 2023/day-04/part_2.py
+++ 2023/day-04/part_2.py
@@ -1,84 +1,106 @@
 #!/usr/bin/env python3
 
 # Advent of Code 2023
-# Day 3, Part 2
+# Day 4, Part 2
 
+import re
 import time
 from datetime import datetime, timedelta
 
-class Schematic:
+def process(card, indent):
+    global cards
 
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.gear_ratios = []
-        self.data = []
+    number = card['number']
+    print(indent + "CARD " + str(number))
+    if number not in counts:
+        counts[number] = 1
+    else:
+        counts[number] += 1
+
+    for my_number in card['my_numbers']:
         
-        with open("input.txt") as input:
-            for line in input:
-                self.data.append(list(line.strip()))
-        
-    def read(self):
-        for y, row in enumerate(self.data):
-            self.y = y
-            for x, cell in enumerate(row):
-                self.x = x
-                if cell == "*":
-                    self.look_around()
-        
-    def look_around(self):
-        print(str(self.x) + "," + str(self.y) + " -> " + self.data[self.y][self.x])
-        print("")
-        print(schematic.data[self.y-1][self.x-1],schematic.data[self.y-1][self.x],schematic.data[self.y-1][self.x+1])
-        print(schematic.data[self.y][self.x-1],schematic.data[self.y][self.x],schematic.data[self.y][self.x+1])
-        print(schematic.data[self.y+1][self.x-1],schematic.data[self.y+1][self.x],schematic.data[self.y+1][self.x+1])
-        print("")
-        
-        gears = []
-        for y in [self.y-1, self.y, self.y+1]:
-            for x in [self.x-1, self.x, self.x+1]:
-                if self.data[y][x].isdigit():
-                    gears.append(int(self.look_back(x,y) + self.data[y][x] + self.look_forward(x,y)))
-        print(gears)
-        if len(gears) == 2:
-            self.gear_ratios.append(gears[0] * gears[1])
-        
-        print("~ ~ ~ ~ ~")
-   
-    def look_back(self, x, y):
-        
-        value = ''
-        x = x-1
-        
-        if self.data[y][x].isdigit():
-            value = str(self.look_back(x, y)) + str(self.data[y][x])
-        
-        return value
-        
-        
-    def look_forward(self, x, y):
-        
-        value = ''
-        x = x+1
-        
-        if x < 140 and self.data[y][x].isdigit():
-            value = str(self.data[y][x]) + str(self.look_forward(x, y))
-            self.data[y][x] = '.'
-        
-        return value
+        if my_number in card['winning_numbers']:
+            number += 1
+            process(cards[number], indent + "...")
 
 starttime = time.time()
 
-schematic = Schematic()
-schematic.read()
-        
+file = "input.txt"
+
+cards = {}
+counts = {}
+
+with open(file) as input:
+    
+    for line in input:
+    
+        number, winning_numbers, my_numbers = line.strip().replace(": ", " | ").split(" | ")
+    
+        number = int(re.findall(r"\d+", number)[0])
+        winning_numbers = re.findall(r"\d+", winning_numbers)
+        my_numbers = re.findall(r"\d+", my_numbers)
+
+        cards[number] = {
+            'number' : number,
+            'winning_numbers' : winning_numbers,
+            'my_numbers' : my_numbers
+        }
+
+for number in cards:
+    process(cards[number], "")
+         
+print("")
+print("EXPECTED: {1: 1, 2: 2, 3: 4, 4: 8, 5: 14, 6: 1}")
+print("ACTUAL..: " + str(counts))
+
 print("")  
-print("Gear ratios sum..: " + str(sum(schematic.gear_ratios)))  
-print("Total runtime....: " + str(round(time.time()-starttime, 3)) + " seconds.")
+print("Total cards....: " + str(sum(counts.values())))  
+print("Total runtime..: " + str(round(time.time()-starttime, 3)) + " seconds.")
 print("")
 
+'''
+SAMPLE DATA
+
+CARD 1
+...CARD 2
+......CARD 3
+.........CARD 4
+............CARD 5
+.........CARD 5
+......CARD 4
+.........CARD 5
+...CARD 3
+......CARD 4
+.........CARD 5
+......CARD 5
+...CARD 4
+......CARD 5
+...CARD 5
+CARD 2
+...CARD 3
+......CARD 4
+.........CARD 5
+......CARD 5
+...CARD 4
+......CARD 5
+CARD 3
+...CARD 4
+......CARD 5
+...CARD 5
+CARD 4
+...CARD 5
+CARD 5
+CARD 6
+
+EXPECTED: {1: 1, 2: 2, 3: 4, 4: 8, 5: 14, 6: 1}
+ACTUAL..: {1: 1, 2: 2, 3: 4, 4: 8, 5: 14, 6: 1}
+
+Total cards....: 30
+Total runtime..: 0.0 seconds.
+'''
 
 '''
-Parts Sum......: 544433
-Total runtime..: 0.005 seconds.
+LIVE DATA
+Total cards....: 5659035
+Total runtime..: 30.878 seconds.
 '''
